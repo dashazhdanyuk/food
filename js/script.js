5864248a42f0c30388ef55f185de2e651c8fc180
Dasha <dashazhdanyuk@gmail.com>
2023-10-01 23:10:29 +0300

    do calculator

--- js/script.js
+++ js/script.js
@@ -459,4 +459,79 @@ window.addEventListener('DOMContentLoaded', () => {
         });
     });
 
+    //Calc
+
+    const result = document.querySelector('.calculating__result span');
+    let sex = 'female',
+        height,
+        weight,
+        age,
+        ratio = 1.375;
+
+    function calcTotal() {
+        if(!sex || !height || !weight || ! age || ! ratio) {
+            result.textContent = '____';
+            return;
+        }
+
+        if (sex === 'female') {
+            result.textContent = Math.round((447.6 + (9.2 * weight) + (3.1 * height) - (4.3 * age)) * ratio);
+        } else {
+            result.textContent = Math.round((88.36 + (13.4 * weight) + (4.8 * height) - (5.7 * age)) * ratio);
+        }
+    }
+
+    calcTotal();
+
+    function getStaticInformation(parentSelector, activeClass){ //функция для дивов активности и выбора male/female
+        const elements = document.querySelectorAll(`${parentSelector} div`); //получаем все дивы с этим классом
+
+        elements.forEach(elem => {
+            elem.addEventListener('click', (e) => { //делегирование событий 
+                if(e.target.getAttribute('data-ratio')){
+                    ratio = +e.target.getAttribute('data-ratio'); //если дивы имеют дата-атрибут с активностью, то собираем с них коеф активности, который указан в дата-аторибуте каждого дива с активностью
+                } else {
+                    sex = e.target.getAttribute('id');
+                }
+    
+                console.log(ratio, sex);
+    
+                //назначаем класс активности (сначала убираем активность у всех)
+                elements.forEach(elem => {
+                    elem.classList.remove(activeClass);
+                });
+    
+                e.target.classList.add(activeClass); //назначаем класс активности диву, в который кликнули
+    
+                calcTotal();
+            });
+        });
+    }
+
+    getStaticInformation('#gender', 'calculating__choose-item_active');
+    getStaticInformation('.calculating__choose_big', 'calculating__choose-item_active');
+
+    function getDynamicInformation(selector) {
+        const input = document.querySelector(selector);
+
+        input.addEventListener('input', () => {
+            switch (input.getAttribute('id')){
+                case 'height':
+                    height = +input.value;
+                    break;
+                case 'weight':
+                    weight = +input.value;
+                    break;
+                case 'age':
+                    age = +input.value;
+                    break;
+            }
+
+            calcTotal();
+        });
+    }
+
+    getDynamicInformation('#height');
+    getDynamicInformation('#weight');
+    getDynamicInformation('#age');
 });
